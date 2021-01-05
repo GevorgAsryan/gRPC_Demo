fd699599e4aa7e006c16819aadca690ab48551fa
GevorgAsryan <Gev.Asryan@gmail.com>
2021-01-05 19:25:31 +0400

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            //var input = new HelloRequest { Name = "Gev" };
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(channel);
+
+            var output1 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            //var output2 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            //var output3 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            //var output4 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            //var output5 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            //var output6 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            //var output7 = await client.GetCustomerInfoAsync(new CustomerLookupModel { UserId = 1});
+            Console.WriteLine(output1);
+            //Console.WriteLine(output2);
+            //Console.WriteLine(output3);
+            //Console.WriteLine(output4);
+            //Console.WriteLine(output5);
+            //Console.WriteLine(output6);
+            //Console.WriteLine(output7);
+            
+            using (var call = client.GetNewCustomers(new NewCostomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine(currentCustomer);
+                }
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,68 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Gevorg";
+                output.Lastname = "Asryan";
+            }
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCostomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Tim",
+                    Lastname = "Corey",
+                    EmailAdress = "tim@iamtimcorey.com",
+                    Age = 41,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Gev",
+                    Lastname = "Asru",
+                    EmailAdress = "gev@iamgivi.com",
+                    Age = 25,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Seto",
+                    Lastname = "Gharibyan",
+                    EmailAdress = "sitros@butelozik.com",
+                    Age = 41,
+                    IsAlive = true
+                }
+            };
+
+            foreach (var cust in customers)
+            {
+                await Task.Delay(2000);
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
